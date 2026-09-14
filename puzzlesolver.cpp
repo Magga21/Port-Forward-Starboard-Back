7434913162953f6c30d1fc353164a636cf70e503
katrinlif95 <katrinlif95@gmail.com>
2026-09-14 13:19:04 +0000

    Tillaga að uppbyggingu forritsins

--- puzzlesolver.cpp
+++ puzzlesolver.cpp
@@ -7,6 +7,25 @@
 #include <iostream>
 #include <unistd.h>
 
+// Helper functions
+// NOT SURE ABOUT THE PARAMETERS!
+void solveSecret(int sockfd, sockaddr_in destaddr) {
+
+}
+
+void solveEvil(int sockfd, sockaddr_in destaddr) {
+
+}
+
+void solveGuardian(int sockfd, sockaddr_in destaddr) {
+
+}
+
+void solveDragon(int sockfd, sockaddr_in destaddr) {
+
+}
+
+// Main function
 int main(int argc, const char* argv[]){
 
 	// Make sure that the function call is correct
@@ -21,6 +40,9 @@ int main(int argc, const char* argv[]){
 	int port2 = std::stoi(argv[3]);
     int port3 = std::stoi(argv[4]);
     int port4 = std::stoi(argv[5]);
+
+    // Array containing all ports from command line
+    int ports[4] = {port1, port2, port3, port4};
 
     // Create UDP socket
 	int sockfd;
@@ -38,10 +60,39 @@ int main(int argc, const char* argv[]){
 		exit(1);
 	}
 
-    // ....
+    // Loop through the ports (array) to find each puzzle
+    for (int i = 0; i < 4; i++) {
+        destaddr.sin_port = htons(ports[i]);
+
+        // Send default message
+        // Recieve response
+        // Check which puzzle it is
+        
+        // NOT SURE ABOUT THE PARAMETERS
+        // if response contains S.E.C.R.E.T?
+        /if () {
+            solveSecret(sockfd, destaddr);
+        }
+
+        // if response contains Evil?
+        else if () {
+            solveEvil(sockfd, destaddr);
+
+        }
+        // if response contains Guardian?
+        else if () {
+            solveGuardian(sockfd, destaddr);
+        }
+
+        // if response contains D.R.A.G.O.N?
+        else if () {
+            solveDragon(sockfd, destaddr);
+        }
+
+        }
 
     
     close(sockfd);
 
-    return 0
+    return 0;
 }	
